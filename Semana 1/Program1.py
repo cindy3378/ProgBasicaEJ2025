2c9ebf265a948f9242b572d051326a6e8025e71c
DESKTOP-NJUOC1L <cindygtz838@gmail.com>
2025-01-27 23:03:31 -0600

    Create Tarea1

--- Semana 1/Program1.py
+++ Semana 1/Program1.py
@@ -1,1 +1,35 @@
+# Ejercicio 1: ¿Cuál es la función de la memoria secundaria en una computadora?
+# c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido.
+
+# Ejercicio 2: ¿Qué es un programa?
+# Es una secuencia de declaraciones o sentencias que han sido diseñadas para hacer algo.
+
+# Ejercicio 3:  ¿Cuál es la diferencia entre un compilador y un intérprete?
+# Un intérprete lee y ejecuta el código fuente del programa directamente, mientras que el intérprete traduce y ejecuta el código sobre la marcha.
+
+# Ejercicio 4: ¿Cuál de los siguientes contiene “código máquina”?
+# a) El intérprete de Python.
+
+# Ejercicio 5: ¿Qué está mal en el siguiente código?:
+# La palabra primt produce un error de sintaxis.
+
+# Ejercicio 6: ¿En qué lugar del computador queda almacenada una variable?
+# b) Memoria Principal
+
+# Ejercicio 7: ¿Qué mostrará en pantalla el siguiente programa?:
+# x = 43
+# x = x + 1
+# print(x)
+# b) 44
+
+# Ejercicio 8: Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+# Unidad Central de Procesamiento: El cerebro porque es el encargado de procesar la información que recibimos, tomar decisiones, etc.
+# Memoria Principal: La memoria a corto plazo porque almacena temporalmente los datos y programas que están siendo utilizados.
+# Memoria Secundaria: La memoria a largo plazo porque es donde se guardan los datos de manera permanente.
+# Dispositivos de Entrada: Los sentidos humanos porque son los que permiten al usuario interactuar con la computadora y proporcionar datos.
+# Dispositivos de Salida: La capacidad de hablar o escribir porque la computadora usa dispositivos de salida para comunicar información.
+
+# Ejercicio 9: ¿Cómo puedes corregir un “Error de sintaxis”?
+# Debes identificar y corregir los problemas relacionados con la forma en que has escrito el código.
+
 print("HOLA MUNDO")
