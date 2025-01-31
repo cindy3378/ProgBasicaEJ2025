577c26653900254ad7ea8a396510710f2c1995a6
DESKTOP-NJUOC1L <cindygtz838@gmail.com>
2025-01-30 20:03:40 -0600

    Change name

---
+++ Semana 2/martes.py
@@ -0,0 +1,9 @@
+Encabezados=['Nombre','FechaNac','LugarNac']
+Datos=['Cindy Carolina Muñoz Gutiérrez','24/02/2007','Monterrey, NL']
+
+cindy=[Encabezados,Datos]
+
+alumno={}
+alumno['2143633']=cindy
+
+print(alumno)
